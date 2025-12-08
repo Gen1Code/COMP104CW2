c00bd54929a853a5c79df981590fe7a1b657ef8c
Gen <72555054+Gen1Code@users.noreply.github.com>
2025-12-08 17:23:36 +0000

    Better Pydriller
    Handle different named "main" branches
    Added commit msg, author name and the branches for a commit,
    within a commit added file change type and changing pathname

--- pydriller/mine.py
+++ pydriller/mine.py
@@ -4,9 +4,30 @@ import json
 import re
 import os
 
-url = "https://github.com/apache/commons-io"
+urls = [
+    "https://github.com/apache/lucene",
+    "https://github.com/apache/commons-io",
+    "https://github.com/apache/commons-lang",
+    "https://github.com/apache/kafka",
+    "https://github.com/apache/hadoop",
+    "https://github.com/apache/cassandra",
+    "https://github.com/apache/tomcat",
+    "https://github.com/apache/maven"
+]
+
 name_pattern = r"/([^/]+)$"
 MANUAL_REMINE = False #Set to true if you want to remine a repo
+
+
+def get_default_branch(repo_path):
+    repo = Repository(repo_path)
+    for commit in repo.traverse_commits():
+        if 'main' in commit.branches:
+            return 'main'
+        elif 'master' in commit.branches:
+            return 'master'
+        print(f"No master or main in {repo_path}")
+        assert False
 
 def save_to_json(repo_url):
     matches = re.findall(name_pattern, repo_url)
@@ -19,18 +40,27 @@ def save_to_json(repo_url):
     if not MANUAL_REMINE and os.path.exists(output_file):
         return
 
-    data = {}
+    main_branch = get_default_branch(repo_url)
+    print(f"Main branch for {matches[0]} is {main_branch}")
 
+    data = {}
+    i = 1
     for commit in Repository(repo_url, 
             only_modifications_with_file_types=['.java'], 
-            only_in_branch="master", 
-            num_workers=4
+            only_in_branch=main_branch,
+            num_workers=8
         ).traverse_commits():
+
+        if i%1000 == 0:
+            print(f"Currently at {i} commits for {matches[0]}")
 
         timestamp = commit.author_date.isoformat()
         commit_data = {
             "timestamp": timestamp,
+            "author": commit.author.name,
             'is_merge': commit.merge,
+            "branches_set": commit.branches,
+            "commit_msg": commit.msg[:100] #Limit to only first 100 characters
         } 
     
         modified_files = []
@@ -38,6 +68,9 @@ def save_to_json(repo_url):
             if modified_file.filename.endswith(".java"):
                 file_data = {
                     "filename": modified_file.filename,
+                    "modification_type": modified_file.change_type, #https://pydriller.readthedocs.io/en/latest/reference.html#pydriller.domain.commit.ModificationType
+                    "new_path": modified_file.new_path,
+                    "old_path": modified_file.old_path,
                 }
                 modified_files.append(file_data)
 
@@ -45,9 +78,12 @@ def save_to_json(repo_url):
 
         data[timestamp] = commit_data
 
+        i+=1
+
     with open(output_file, 'w', encoding='utf-8') as f:
         json.dump(data, f, ensure_ascii=False)
     print(f"saved {repo_url} to {output_file}")
 
 
-save_to_json(url)
+for url in urls:
+    save_to_json(url)
